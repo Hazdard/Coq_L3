088aacacaf2efa74adc149c2d63fa4c22314f3aa
Hazdard <sacha.ben-arous@ens-paris-saclay.fr>
2023-03-28 21:49:42 +0200

    xeule2

--- semantique.v
+++ semantique.v
@@ -17,7 +17,7 @@ Fixpoint interp_earith (e : earith) (vars : nat -> Z) : Z := match e with
   | Moins e1 => - (interp_earith e1 vars)
 end.
 
-Lemma deterermin : forall (e:earith) (vars : nat -> Z) (a b : Z), (interp_earith e vars) = a -> (interp_earith e vars) = b -> a=b.
+Lemma determin_arith : forall (e:earith) (vars : nat -> Z) (a b : Z), (interp_earith e vars) = a -> (interp_earith e vars) = b -> a=b.
 Proof.
 intros. rewrite H in H0. exact H0.
 Qed.
@@ -40,3 +40,56 @@ Fixpoint interp_ebool (e : e_bool) (vars : nat -> Z) : Z := match e with
   | Notb e => if Z.eqb (interp_ebool e vars) 0 then 0 else (interp_ebool e vars)
   | Andb e1 e2 => if Z.eqb (interp_ebool e1 vars) 0 then 0 else (interp_ebool e2 vars)
 end.
+
+Lemma determin_bool : forall (e:e_bool) (vars : nat -> Z) (a b : Z), (interp_ebool e vars) = a ->(interp_ebool e vars) = b -> a=b.
+Proof.
+intros. rewrite H in H0. exact H0.
+Qed.
+
+(** Exercice 3 **)
+
+Inductive IMP :=
+  | Skip : IMP
+  | Affect : nat -> earith -> IMP
+  | Cons : IMP -> IMP -> IMP
+  | IfThenElse : e_bool -> IMP -> IMP -> IMP
+  | While : e_bool -> IMP -> IMP.
+
+Fixpoint interp_imp (e : IMP) (vars : nat -> Z) (i : nat): (nat -> Z) := 
+  match i with 
+    | 0=> vars
+    | S l => match e with
+                  | Skip => vars
+                  | Affect n e => fun (k : nat) => if n=?k then (interp_earith e vars) else (vars k)
+                  | Cons e1 e2 => interp_imp e2 (interp_imp e1 vars l) l
+                  | IfThenElse bl e1 e2 => if Z.eqb (interp_ebool bl vars) 0 then (interp_imp e2 vars l) else (interp_imp e1 vars l)
+                  | While bl e1 => if Z.eqb (interp_ebool bl vars) 0 then vars else (interp_imp (While bl e1) (interp_imp e1 vars l) l)
+                                    end
+                end.
+
+Lemma determin_imp : forall (e:IMP) (vars : nat -> Z) (i : nat) (a b : nat -> Z), (interp_imp e vars i) = a -> (interp_imp e vars i) = b -> a=b.
+Proof.
+intros. rewrite H in H0. exact H0.
+Qed.
+
+Definition equivalent_com (e1 e2 : IMP ) : Prop := exists i, forall (vars : nat -> Z), (interp_imp e1 vars (S i)) = (interp_imp e2 vars (S i)).
+
+
+Goal forall (b : e_bool) (c : IMP), equivalent_com (While b c) (IfThenElse b (Cons c (While b c)) Skip).
+Proof.
+intros.
+unfold equivalent_com. eexists. intros. unfold interp_imp. induction (interp_ebool b vars).
+    + simpl. instantiate (1 := 0). simpl. reflexivity.
+    + simpl. reflexivity.
+    + simpl. reflexivity.
+Qed.
+
+
+
+
+
+
+
+
+
+
