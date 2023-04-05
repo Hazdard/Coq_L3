02af7872cde4b2ee5b584536345b3e54b67c2e97
Hazdard <sacha.ben-arous@ens-paris-saclay.fr>
2023-04-05 19:08:29 +0200

    xeule2

--- semantique.v
+++ semantique.v
@@ -1,116 +1,156 @@
 Require Import ZArith.
 
-(* Search (Z -> Z -> bool). *)
 
 (** Exercice 1 **)
 
-Inductive earith :=
-  | Const : Z -> earith
-  | Var : nat -> earith
-  | Plus : earith -> earith -> earith
-  | Mult : earith -> earith -> earith
-  | Moins : earith -> earith.
 
-Fixpoint interp_earith (e : earith) (vars : nat -> Z) : Z := match e with
-  | Const n => n
-  | Var n => vars n
-  | Plus e1 e2 => (interp_earith e1 vars) + (interp_earith e2 vars)
-  | Mult e1 e2 => (interp_earith e1 vars) * (interp_earith e2 vars)
-  | Moins e1 => - (interp_earith e1 vars)
-end.
+  (** Question 1**) 
 
-Lemma determin_arith : forall (e:earith) (vars : nat -> Z) (a b : Z), (interp_earith e vars) = a -> (interp_earith e vars) = b -> a=b.
-Proof.
-intros. rewrite H in H0. exact H0.
-Qed.
+  Inductive env :=
+    | Bot : env
+    | Env : (nat -> Z) -> env.
 
-(** Exercice 2 **)
+  Inductive earith :=
+    | Const : Z -> earith
+    | Var : nat -> earith
+    | Plus : earith -> earith -> earith
+    | Mult : earith -> earith -> earith
+    | Moins : earith -> earith.
 
-Inductive e_bool :=
-  | Constb : Z -> e_bool
-  | Varb : nat -> e_bool
-  | Eqb : e_bool -> e_bool -> e_bool
-  | Notb : e_bool -> e_bool
-  | Andb : e_bool -> e_bool -> e_bool.
+  Inductive sem_earith : earith -> env -> Z -> Prop :=
+    | SemConst : forall (n : Z) (vars : nat -> Z), sem_earith (Const n) (Env vars) n
+    | SemVars : forall (x : nat) (vars : nat -> Z), sem_earith (Var x) (Env vars) (vars x)
+    | SemPlus : forall (e1 e2 : earith) (vars : nat-> Z) (n1 n2 : Z), sem_earith e1 (Env vars) n1 -> sem_earith e2 (Env vars) n2 -> sem_earith (Plus e1 e2) (Env vars) (n1 + n2)
+    | SemMult : forall (e1 e2 : earith) (vars : nat-> Z) (n1 n2 : Z), sem_earith e1 (Env vars) n1 -> sem_earith e2 (Env vars) n2 -> sem_earith (Mult e1 e2) (Env vars) (n1 * n2)
+    | SemMoins : forall (e1 : earith) (vars : nat-> Z) (n1 n2 : Z), sem_earith e1 (Env vars) n1 -> sem_earith (Moins e1) (Env vars) (- n1).
 
-Search (Z -> Z -> bool).
+  (** Question 2 **)
 
-Fixpoint interp_ebool (e : e_bool) (vars : nat -> Z) : Z := match e with
-  | Constb n => n
-  | Varb n => vars n
-  | Eqb e1 e2 => if Z.eqb (interp_ebool e1 vars) (interp_ebool e2 vars) then (interp_ebool e1 vars) else 0
-  | Notb e => if Z.eqb (interp_ebool e vars) 0 then 0 else (interp_ebool e vars)
-  | Andb e1 e2 => if Z.eqb (interp_ebool e1 vars) 0 then 0 else (interp_ebool e2 vars)
-end.
-
-Lemma determin_bool : forall (e:e_bool) (vars : nat -> Z) (a b : Z), (interp_ebool e vars) = a ->(interp_ebool e vars) = b -> a=b.
-Proof.
-intros. rewrite H in H0. exact H0.
-Qed.
-
-(** Exercice 3 **)
-
-Inductive IMP :=
-  | Skip : IMP
-  | Affect : nat -> earith -> IMP
-  | Cons : IMP -> IMP -> IMP
-  | IfThenElse : e_bool -> IMP -> IMP -> IMP
-  | While : e_bool -> IMP -> IMP
-  | Until : e_bool -> IMP -> IMP
-  | DoubleAffect : nat -> nat -> earith -> earith -> IMP.
-
-Fixpoint interp_imp (e : IMP) (vars : nat -> Z) (i : nat): (nat -> Z) := 
-  match i with 
-    | 0=> vars
-    | S l => match e with
-                  | Skip => vars
-                  | Affect n e => fun (k : nat) => if k=?n then (interp_earith e vars) else (vars k)
-                  | Cons e1 e2 => interp_imp e2 (interp_imp e1 vars l) l
-                  | IfThenElse bl e1 e2 => if Z.eqb (interp_ebool bl vars) 0 then (interp_imp e2 vars l) else (interp_imp e1 vars l)
-                  | While bl e1 => if Z.eqb (interp_ebool bl vars) 0 then vars else (interp_imp (While bl e1) (interp_imp e1 vars l) l)
-                  | Until bl e1 => if Z.eqb (interp_ebool bl vars) 0 then (interp_imp (While bl e1) (interp_imp e1 vars l) l) else vars
-                  | DoubleAffect n1 n2 e1 e2 => fun (k : nat) => if k=?n2 then (interp_earith e2 vars) else (if k=?n1 then (interp_earith e1 vars) else (vars k))
+  Fixpoint interp_earith (e : earith) (vars : env) : Z := match vars with
+  | Bot => 667
+  | Env env1 => match e with
+                | Const n => n
+                | Var n => env1 n
+                | Plus e1 e2 => (interp_earith e1 vars) + (interp_earith e2 vars)
+                | Mult e1 e2 => (interp_earith e1 vars) * (interp_earith e2 vars)
+                | Moins e1 => - (interp_earith e1 vars)
                 end
 end.
 
-Lemma determin_imp : forall (e:IMP) (vars : nat -> Z) (i : nat) (a b : nat -> Z), (interp_imp e vars i) = a -> (interp_imp e vars i) = b -> a=b.
-Proof.
-intros. rewrite H in H0. exact H0.
+  (** Question 3 **)
+
+  Lemma determin_earith : forall (e:earith) (vars : env) (a b : Z), sem_earith e vars a -> sem_earith e vars b -> a=b.
+  Proof.
+  induction e.
+    + intros. inversion H. inversion H0. subst. reflexivity.
+    + intros. inversion H. inversion H0. subst. inversion H4. reflexivity.
+    + intros. inversion H. inversion H0. subst. inversion H10. rewrite H2 in H9. pose proof (IHe1 (Env vars0) n1 n0 H3 H9). rewrite H2 in H12. pose proof (IHe2 (Env vars0) n2 n3 H6 H12). rewrite H1. rewrite H4. reflexivity.
+    + intros. inversion H. inversion H0. subst. inversion H10. rewrite H2 in H9. pose proof (IHe1 (Env vars0) n1 n0 H3 H9). rewrite H2 in H12. pose proof (IHe2 (Env vars0) n2 n3 H6 H12). rewrite H1. rewrite H4. reflexivity.
+    + intros. inversion H. inversion H0. subst. inversion H7.  rewrite H3 in H6. pose proof (IHe (Env vars0) (n1) (n0) H2 H6). rewrite H1. reflexivity.
 Qed.
 
-Definition equivalent_com (e1 e2 : IMP ) : Prop := exists i, forall (vars : nat -> Z), (interp_imp e1 vars (S i)) = (interp_imp e2 vars (S i)).
+
+(** Exercice 2 **) 
+
+  (** Question 1 **)
+
+  Inductive e_bool :=
+    | Constb : Z -> e_bool
+    | Varb : nat -> e_bool
+    | Equalb : earith -> earith -> e_bool
+    | Notb : e_bool -> e_bool
+    | Andb : e_bool -> e_bool -> e_bool.
+
+  Inductive sem_e_bool : e_bool -> env -> Z -> Prop :=
+    | SemConstb : forall (n : Z) (vars : nat -> Z), sem_e_bool (Constb n) (Env vars) n
+    | SemVarb : forall (x : nat) (vars : nat -> Z), sem_e_bool (Varb x) (Env vars) (vars x)
+    | SemEqualb : forall (e1 e2 : earith) (vars : nat-> Z) (n1 n2 : Z),
+        sem_earith e1 (Env vars) n1 -> sem_earith e2 (Env vars) n2 -> sem_e_bool (Equalb e1 e2) (Env vars) (if Z.eqb n1 n2 then 1 else 0) 
+    | SemNotb : forall (e : e_bool) (vars : nat -> Z) (n : Z),
+        sem_e_bool e (Env vars) n -> sem_e_bool (Notb e) (Env vars) (if Z.eqb n 0 then 42 else 0)
+    | SemAndb : forall (e1 e2 : e_bool) (vars : nat-> Z) (n1 n2 : Z),
+        sem_e_bool e1 (Env vars) n1 -> sem_e_bool e2 (Env vars) n2 -> sem_e_bool (Andb e1 e2) (Env vars) (if Z.eqb (n1*n2) 0 then 0 else 42).
+
+  (** Question 2 **)
+
+  Fixpoint interp_e_bool (e : e_bool) (vars : env) : Z := match vars with
+    | Bot => 0
+    | Env env1 => match e with
+                  | Constb n => n
+                  | Varb n => env1 n
+                  | Equalb e1 e2 => if Z.eqb (interp_earith e1 vars) (interp_earith e2 vars) then 1 else 0
+                  | Notb e => if Z.eqb(interp_e_bool e vars) (Z.of_nat 0) then 1 else 0
+                  | Andb e1 e2 => if orb (Z.eqb (interp_e_bool e1 vars) 0) (Z.eqb (interp_e_bool e2 vars) 0) then 0 else 1
+                end
+  end.
+
+  (** Question 3 **)
+
+  Theorem determin_e_bool : forall (e : e_bool) (vars : env) (a b : Z), sem_e_bool e vars a -> sem_e_bool e vars b -> a = b.
+  Proof.
+    induction e.
+    + intros. inversion H. inversion H0. subst. reflexivity.
+    + intros. inversion H. inversion H0. subst. inversion H4. reflexivity.
+    + intros. inversion H. inversion H0. subst. rewrite H10 in H9. rewrite H10 in H12.
+      pose proof (determin_earith e (Env vars0) n1 n0 H3 H9).
+      pose proof (determin_earith e0 (Env vars0) n2 n3 H6 H12).
+      rewrite H1. rewrite H2. reflexivity.
+    + intros. inversion H. inversion H0. subst. rewrite H7 in H6.
+      pose proof (IHe (Env vars0) n n0 H2 H6).
+      rewrite H1. reflexivity.
+    + intros. inversion H. inversion H0. subst. rewrite H10 in H9. rewrite H10 in H12.
+      pose proof (IHe1 (Env vars0) n1 n0 H3 H9).
+      pose proof (IHe2 (Env vars0) n2 n3 H6 H12).
+      rewrite H1. rewrite H2. reflexivity.
+  Qed.
 
 
-Goal forall (b : e_bool) (c : IMP), equivalent_com (While b c) (IfThenElse b (Cons c (While b c)) Skip).
-Proof.
-intros.
-unfold equivalent_com. eexists. intros. unfold interp_imp. induction (interp_ebool b vars).
-    + simpl. instantiate (1 := 0). simpl. reflexivity.
-    + simpl. reflexivity.
-    + simpl. reflexivity.
-Qed.
+(** Exercice 3 **)
 
-Axiom fun_eq : forall (f g : nat -> Z), (forall x : nat, f x = g x) -> f = g.
+  (** Question 1 **)
 
-Lemma fun_eq_inv : forall (x :nat) (f g : nat -> Z), f = g -> f x = g x.
-Proof.
-intros. rewrite H. reflexivity.
-Qed.
+  Inductive IMP :=
+    | Skip : IMP
+    | Affect : nat -> earith -> IMP
+    | Cons : IMP -> IMP -> IMP
+    | IfThenElse : e_bool -> IMP -> IMP -> IMP
+    | While : e_bool -> IMP -> IMP
+    | Until : e_bool -> IMP -> IMP
+    | DoubleAffect : nat -> nat -> earith -> earith -> IMP.
 
-Goal forall (x y :nat) (e1 e2 : earith),
-(forall (vars : nat-> Z), (interp_earith e2 vars) = (interp_earith e2 (fun k : nat => if k =? x then interp_earith e1 vars else vars k)))
-<->
-(equivalent_com (DoubleAffect x y e1 e2) (Cons (Affect x e1) (Affect y e2))).
-Proof. intros. split.
-    + exists 1. intros. apply (fun_eq (interp_imp (DoubleAffect x y e1 e2) vars 2) (interp_imp (Cons (Affect x e1) (Affect y e2)) vars 2)). intros. simpl. destruct (x0 =? y).
-      ++ apply (H vars).
-      ++ reflexivity.
-    + unfold equivalent_com. intros. destruct H as [i H']. pose proof (H' vars). apply (fun_eq_inv y) in H. repeat simpl in H. destruct (y =? y).
-      ++ unfold interp_imp in H. destruct i.
-        (* i=0 *) +++ pose proof (H' (fun (k:nat) => if k=?x then (Z.of_nat 1) else (if k=?y then (Z.of_nat 2) else (Z.of_nat 0)))). simpl in H0. pose proof (fun_eq H0 x).
+Definition update_env (vars : env) (x : nat) (n : Z) : env := match vars with
+  |Bot => Bot 
+  |Env env1 => Env (fun (k:nat) => if k=?x then n else (env1 k))
+end.
 
+  Inductive sem_imp : IMP -> env -> env -> Prop :=
+    | SemSkip : forall (vars : env), sem_imp Skip vars vars
+    | SemAffect : forall (x : nat) (e : earith) (n : Z) (vars : env), sem_earith e vars n -> sem_imp (Affect x e) vars (update_env vars x n)
+    | SemCons : forall (vars1 vars2 vars3 : env) (e1 e2 : IMP), sem_imp e1 vars1 vars2 -> sem_imp e2 vars2 vars3 -> sem_imp (Cons e1 e2) vars1 vars3
+    | SemIfThen : forall (vars1 vars2 : env) (e1 e2 : IMP) (b : e_bool) (n : Z), sem_e_bool b vars1 n -> n <> 0%Z -> sem_imp e1 vars1 vars2 -> sem_imp (IfThenElse b e1 e2) vars1 vars2
+    | SemifElse : forall (vars1 vars2 : env) (e1 e2 : IMP) (b : e_bool) (n : Z), sem_e_bool b vars1 n -> n = 0%Z -> sem_imp e2 vars1 vars2 -> sem_imp (IfThenElse b e1 e2) vars1 vars2
+    | SemWhileIn : forall (vars1 vars2 : env) (e : IMP) (b : e_bool) (n : Z), sem_e_bool b vars1 n -> n <> 0%Z -> sem_imp e vars1 vars2 -> sem_imp (While b e) vars1 vars2
+    | SemWhileOut : forall (vars1 vars2 : env) (e : IMP) (b : e_bool) (n : Z), sem_e_bool b vars1 n -> n = 0%Z -> sem_imp (While b e) vars1 vars1
+   (** Question 6 **)
+   | SemUntilIn : forall (vars1 vars2 : env) (e : IMP) (b : e_bool) (n : Z), sem_e_bool b vars1 n -> n = 0%Z -> sem_imp e vars1 vars2 -> sem_imp (Until b e) vars1 vars2
+    | SemUntilOut : forall (vars1 vars2 : env) (e: IMP) (b : e_bool) (n : Z), sem_e_bool b vars1 n -> n <> 0%Z -> sem_imp (Until b e) vars1 vars1
+    (** Question 7 **)
+    | SemDoubleAffect : forall (x y : nat) (e1 e2 : earith) (n1 n2 : Z) (vars : env), sem_earith e1 vars n1 -> sem_earith e2 vars n2  -> sem_imp (DoubleAffect x y e1 e2) vars (update_env (update_env vars x n1) y n2).
 
+  (** Question 2 **)
 
-
-
-
+  Fixpoint interp_imp (e : IMP) (vars : env) (i : nat): env := match vars with 
+    | Bot => Bot
+    | Env env1 => match i with 
+                        | 0=> Bot
+                        | S l => match e with
+                                      | Skip => vars
+                                      | Affect x e => (update_env vars x (interp_earith e vars))
+                                      | Cons e1 e2 => interp_imp e2 (interp_imp e1 vars l) l
+                                      | IfThenElse bl e1 e2 => if Z.eqb (interp_e_bool bl vars) 0 then (interp_imp e2 vars l) else (interp_imp e1 vars l)
+                                      | While bl e1 => if Z.eqb (interp_e_bool bl vars) 0 then vars else (interp_imp (While bl e1) (interp_imp e1 vars l) l)
+                                      | Until bl e1 => if Z.eqb (interp_e_bool bl vars) 0 then (interp_imp (While bl e1) (interp_imp e1 vars l) l) else vars
+                                      | DoubleAffect x y e1 e2 => (update_env (update_env vars x (interp_earith e1 vars)) y (interp_earith e2 vars))
+                                end
+                  end
+end.
